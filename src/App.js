5ef0fe6bdfe3d6f96396085a3cd1098a0a3be67b
EC2 Default User <ec2-user@ip-172-31-14-190.us-west-1.compute.internal>
2021-09-11 16:09:19 +0000

    alpha-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+const ToDoItem = (props) => {
+  const [isDone, setIsDone] = useState(false)
+  const item = props.item
+  const deleteList = props.deleteList
+  const handleClick = (e) => {
+    if (isDone){
+      setIsDone(false)
+    } else {
+      setIsDone(true)
+    }
+  }
+  const handleBtnClick = (e) => {
+    deleteList(item)
+  }
+
+  let displayText = item
+  if(isDone){
+    displayText = item + ' finished'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <li onClick={handleClick}>{displayText}</li>
+      <button onClick={handleBtnClick}>Delete</button>
     </div>
-  );
+  )
 }
 
-export default App;
+const ToDoList = (props) => {
+  const toDoList = props.toDoList
+  const toDoListComponent = toDoList.map(item => {
+    return (
+      <ToDoItem key={item} deleteList={props.deleteList} item={item}/>
+    )
+  })
+  return (
+    <ul>{toDoListComponent}</ul>
+  )
+}
+
+const Input = (props) => {
+  const [name, setName] = useState('')
+  const addList = props.addList
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+  const handleClick = (e) => {
+    addList(name)
+    setName('')
+  }
+  return (
+    <div>
+      <input onChange={handleChange} value={name}/>
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  )
+}
+
+const ToDoApp = (props) => {
+  const [toDoList, setToDoList] = useState([])
+  const addList = (newItem) => {
+    setToDoList([...toDoList, newItem])
+  }
+  const deleteList = (del) => {
+    const newList = toDoList.filter(item => {
+      return item !== del
+    })
+    setToDoList(newList)
+  }
+
+  return (
+    <div>
+      <ToDoList toDoList={toDoList} deleteList={deleteList}/>
+      <Input addList={addList}/>
+    </div>
+  )
+}
+
+export default ToDoApp;
